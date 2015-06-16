1a57cd4277ea40837abd5963c7a2214fab690105
kuangzhexi <sars.715@gmail.com>
2015-06-16 23:13:09 +0800

    Initial commit

---
+++ simplex_method/simplex_method.py
@@ -0,0 +1,18 @@
+__author__ = 'kuangzhexi'
+
+import numpy as np
+import sys
+
+
+class SimplexMethod(object):
+    def __init__(self):
+        self.__c = np.matrix([])
+        self.__A = np.matrix([])
+        self.__b = np.matrix([])
+
+    def eval(self):
+        pass
+
+if __name__ == '__main__':
+    for line in sys.stdin:
+        pass
